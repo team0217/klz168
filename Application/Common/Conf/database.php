a5a83d1909be95ac4d64fb5a1615ce87fa1181ac
ck <ck@163.com>
2017-03-18 11:42:22 +0800

    11111

--- Application/Common/Conf/database.php
+++ Application/Common/Conf/database.php
@@ -3,9 +3,9 @@ return array(
 	// 数据库配置
   	'DB_TYPE'               =>  'Mysql',     // 数据库类型
     'DB_HOST'               =>  '127.0.0.1', 	// 服务器地址
-    'DB_NAME'               =>  'xuewl',          // 数据库名
+    'DB_NAME'               =>  'test',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '655bfa668b7a24a7acdbaa0856b4317f',          // 密码
+    'DB_PWD'                =>  '123456',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'xw_',         // 数据库表前缀
     'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
