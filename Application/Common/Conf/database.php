370812176eccaad10b5b5f471267ba6a8e67fa03
ck <ck@163.com>
2017-03-21 21:32:21 +0800

    1

--- Application/Common/Conf/database.php
+++ Application/Common/Conf/database.php
@@ -14,7 +14,7 @@ return array(
     'DB_DEPLOY_TYPE'        =>  0, // 数据库部署方式:0 集中式(单一服务器),1
 	
 	
-	'REDIS_HOST' =>'r-bp1d1695b6fcfe44.redis.rds.aliyuncs.com',
+	'REDIS_HOST' =>'118.190.41.0',
 	'REDIS_PORT' =>'6379',
-	'REDIS_PWD' =>'123456Tt',
+	'REDIS_PWD' =>'123456789',
 );
