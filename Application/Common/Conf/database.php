aa15878a10456f90ec372bd4506e008f8d5d4422
ck <ck@163.com>
2017-03-21 21:06:13 +0800

    1

--- Application/Common/Conf/database.php
+++ Application/Common/Conf/database.php
@@ -5,7 +5,7 @@ return array(
     'DB_HOST'               =>  '127.0.0.1', 	// 服务器地址
     'DB_NAME'               =>  'test',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '123456',          // 密码
+    'DB_PWD'                =>  '1234561',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'xw_',         // 数据库表前缀
     'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
@@ -14,7 +14,7 @@ return array(
     'DB_DEPLOY_TYPE'        =>  0, // 数据库部署方式:0 集中式(单一服务器),1
 	
 	
-	'REDIS_HOST' =>'r-wz9053c70d6df154.redis.rds.aliyuncs.com',
-	'REDIS_PORT' =>'6379aaa',
-	'REDIS_PWD' =>'qweqwqwWQEWEQWE12123WEQW',
+	'REDIS_HOST' =>' r-bp1d1695b6fcfe44.redis.rds.aliyuncs.com',
+	'REDIS_PORT' =>'6379',
+	'REDIS_PWD' =>'123456',
 );
